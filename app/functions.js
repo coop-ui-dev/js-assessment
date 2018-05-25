f380cc52b84776369988adf41aa3ce0c2f0d5410
coop-ui-dev <39286716+coop-ui-dev@users.noreply.github.com>
2018-05-25 18:08:18 -0500

    Update functions.js

--- app/functions.js
+++ app/functions.js
@@ -10,7 +10,8 @@ functionsAnswers = {
    * @returns Whatever fn would return
    */
   argsAsArray: function argsAsArray(fn, arr) {
-
+ 
+    return fn.apply(this, arr);
   },
 
   /**
@@ -27,6 +28,8 @@ functionsAnswers = {
    * that begins with str + ', ' + that 1 argument.
    */
   functionFunction: function functionFunction(str) {
+
+    return function(appendStr) { return str + ", " + appendStr; }
 
   },
 };
