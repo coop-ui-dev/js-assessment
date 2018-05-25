42c5165bfa3ced28b5ce7d05251b28e5011a2507
coop-ui-dev <39286716+coop-ui-dev@users.noreply.github.com>
2018-05-25 18:09:34 -0500

    Update strings.js

--- app/strings.js
+++ app/strings.js
@@ -1,4 +1,7 @@
 stringsAnswers = {
+
+  useBuiltin : false,
+
   /**
    * Reduces a string by removing letters that repeat more than amount times.
    * 
@@ -11,6 +14,25 @@ stringsAnswers = {
    */
   reduceString: function reduceString(str, amount) {
 
+    //---
+    //  NOTE : this is a good problem.
+    //---
+
+    //---
+    //  Going to take a big hammer approach to this.
+    //
+    //  Depending on the size of the string - there are much better ways
+    //  to accomplish this.
+    //---
+    for(var index = 0; index < str.length; index++) {
+      var temp = Array(amount + 1).fill(str[index]).join('');
+      var loc = str.indexOf(temp);
+      while(loc > -1) {
+        str = str.replace(temp, temp.substr(0, amount));
+        loc = str.indexOf(temp);
+      }
+    }
+    return str;
   },
 
   /**
@@ -23,5 +45,15 @@ stringsAnswers = {
    */
   reverseString: function reverseString(str) {
 
+    if(this.useBuiltin) {
+      return str.split('').reverse().join('');
+    } else {
+      var len = str.length - 1;
+      var retValue = "";
+      for(var index = len; index >= 0; index--) {
+        retValue += str[index];
+      }
+      return retValue;
+    }
   },
 };
